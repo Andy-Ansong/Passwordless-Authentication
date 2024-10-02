696a00d0dd23fbf7cfb6ab44d61be4f533c89de6
IT Compliance <compliance@amalitech.org>
2024-10-02 15:51:50 +0000

    update readme

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -22,7 +22,9 @@ const transporter = nodemailer.createTransport({
 });
 userRouter.get("/me", auth, async (req, res) => {
     try {
-        const user = await Employee.findOne({name: "Andy Ansong"}).exec()
+        const token = req.header("Authorization").replace("Bearer ", "")
+        const data = jwt.verify(token, process.env.JWT_KEY)
+        const user = Employee.find({_id: data._id})
         res.status(200).json({ user });
     } catch (err) {
         res.status(404).json({ error: "User not found" });
@@ -62,8 +64,7 @@ userRouter.post("/request-code", async (req, res) => {
 
         res.status(200).send({
             status: "success",
-            message: "A one-time code has been sent to your email address.",
-            otp: otp
+            message: "A one-time code has been sent to your email address."
         })
     }catch(error){
         res.status(400).send({
