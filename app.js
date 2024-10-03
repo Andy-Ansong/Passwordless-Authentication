54c462c55c3827de20f285042ed8162a289da3da
IT Compliance <compliance@amalitech.org>
2024-10-03 16:28:32 +0000

    create profiles route

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.use(express.json())
 app.use(require('body-parser').json())
 
 const authRouter = require("./routes/authRouter")
-app.use("/auth", authRouter)
+app.use("/api/v1/auth", authRouter)
 const profileRouter = require("./routes/profileRouter")
-app.use("/profile", profileRouter)
+app.use("/api/v1/profile", profileRouter)
 
 app.listen(port, () => {
     console.log("Running on port ", port)
--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -3,15 +3,57 @@ const Profile = require('../model/Profile')
 const profileRouter = express.Router()
 const auth = require('../middleware/auth')
 
+// create a profile
+profileRouter.post("/", async (req, res) => {
+    try{
+        const profile = new Profile(req.body)
+        await profile.save()
+        res.status(201).send({message: "Profile saved successfully", profile})
+    }catch(err){
+        res.send({error: "There was a problem saving data. Please try again"})
+    }
+})
+
 // get all profiles
 profileRouter.get("/", async (req, res) => {
     const profiles = await Profile.find({})
-    res.send({profiles})
+    res.status(200).send({profiles})
 })
 
-// create a profile
-profileRouter.post("/", async (req, res) => {
-    res.send({message: "Working"})
+// get single profile
+profileRouter.get(":profile_id", async (req, res) => {
+    try{
+        const profile_id = req.params.profile_id
+        const profile = await Profile.findById(profile_id)
+        res.status(200).send({profile})
+    }catch(err){
+        res.status(404).send({error: "Profile not found"})
+    }
+})
+
+// update profile by id
+profileRouter.put("/:id", async (req, res) => {
+    try{
+        const id = req.params.id
+        const updatedProfile = req.body;
+        const profile = await Profile.findByIdAndDelete(id)
+        profile.save()
+        res.status(204).send()
+    }catch(err){
+        res.status(404).send({error: "Profile not found"})
+    }
+})
+
+// delete profile by id
+profileRouter.delete("/:profile_id", async (req, res) => {
+    try{
+        const profile_id = req.params.profile_id
+        const profile = await Profile.findByIdAndDelete(profile_id)
+        profile.save()
+        res.status(204).send()
+    }catch(err){
+        res.status(404).send({error: "Profile not found"})
+    }
 })
 
 module.exports = profileRouter
